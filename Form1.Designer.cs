342a0db84c8069acc96991251898b3d3e4a2b946
Lackneets <lackneets@gmail.com>
2016-02-09 14:32:34 +0800

    Archived 2011-12-25

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace osuRingtoneRipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_saveRingtone = new System.Windows.Forms.Button();
+            this.songsList = new System.Windows.Forms.ListBox();
+            this.songImage = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_random = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.songImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_saveRingtone
+            // 
+            this.btn_saveRingtone.Location = new System.Drawing.Point(804, 429);
+            this.btn_saveRingtone.Name = "btn_saveRingtone";
+            this.btn_saveRingtone.Size = new System.Drawing.Size(106, 23);
+            this.btn_saveRingtone.TabIndex = 1;
+            this.btn_saveRingtone.Text = "儲存為手機鈴聲";
+            this.btn_saveRingtone.UseVisualStyleBackColor = true;
+            this.btn_saveRingtone.Click += new System.EventHandler(this.btn_saveRingtone_Click);
+            // 
+            // songsList
+            // 
+            this.songsList.BackColor = System.Drawing.SystemColors.Window;
+            this.songsList.FormattingEnabled = true;
+            this.songsList.ItemHeight = 12;
+            this.songsList.Location = new System.Drawing.Point(12, 40);
+            this.songsList.Name = "songsList";
+            this.songsList.Size = new System.Drawing.Size(294, 412);
+            this.songsList.TabIndex = 2;
+            this.songsList.SelectedIndexChanged += new System.EventHandler(this.songsList_SelectedValueChanged);
+            this.songsList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.songsList_MouseDoubleClick);
+            // 
+            // songImage
+            // 
+            this.songImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.songImage.Image = global::osuRingtoneRipper.Properties.Resources.img1452816;
+            this.songImage.Location = new System.Drawing.Point(323, 16);
+            this.songImage.Name = "songImage";
+            this.songImage.Size = new System.Drawing.Size(587, 436);
+            this.songImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.songImage.TabIndex = 3;
+            this.songImage.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "敬請期待篩選功能";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_random
+            // 
+            this.btn_random.Location = new System.Drawing.Point(723, 429);
+            this.btn_random.Name = "btn_random";
+            this.btn_random.Size = new System.Drawing.Size(75, 23);
+            this.btn_random.TabIndex = 5;
+            this.btn_random.Text = "隨機歌曲";
+            this.btn_random.UseVisualStyleBackColor = true;
+            this.btn_random.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.OrangeRed;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.SystemColors.ControlDark;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 455);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(282, 12);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "osu! RingtoneRipper 0.1 beta © 2011 Lackneets 小耀博士";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.SystemColors.ControlDark;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(922, 474);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btn_random);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.songsList);
+            this.Controls.Add(this.btn_saveRingtone);
+            this.Controls.Add(this.songImage);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "osu! RingtoneRipper ";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.songImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btn_saveRingtone;
+        private System.Windows.Forms.ListBox songsList;
+        private System.Windows.Forms.PictureBox songImage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_random;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+
+namespace osuRingtoneRipper
+{
+
+
+    public partial class Form1 : Form
+    {
+     
+
+        private bool closing = false;
+        private Thread playerThread;
+        private string playerSong = "F:/Dropbox/Friends/天富❤駿麒❤花爺/supercell feat. HATSUNE MIKU/08 初めての恋が終わる時.mp3";
+        private int playerPreviewTime;
+
+        string osuSongsDir = "-1";
+
+        DataTable songlistDataTable = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            btn_saveRingtone.Enabled = false;
+             
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            locateOsuPath(); 
+        }
+
+        private void locateOsuPath(){
+            string[] candidateDisk = { "C:/", "D:/", "E:/", "F:/", "G:/", "H:/" };
+            string[] candidatePath = { "Program Files (x86)/osu!", "Program Files/osu!", "osu!" };
+            
+            foreach(string disk in candidateDisk){
+                foreach (string path in candidatePath) {
+                    string songpath = disk + path + "/Songs/";
+                    if (Directory.Exists(songpath) || osuSongsDir != "-1"){ 
+                        osuSongsDir = songpath;
+                        break;
+                    }
+                }
+                if (osuSongsDir != "-1") break;
+            }
+
+            if (osuSongsDir == "-1") {
+                MessageBox.Show("找不到您的 osu! 請手動選擇 osu! 安裝的目錄！");
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    osuSongsDir = fbd.SelectedPath + "/Songs/";
+                }
+            }
+
+            if (!Directory.Exists(osuSongsDir)) {
+                MessageBox.Show("無法讀取 osu! 歌曲");
+                System.Environment.Exit(0);
+            }
+
+            try
+            {
+
+                string[] songsDirs = Directory.GetDirectories(osuSongsDir);
+
+
+                songlistDataTable.Columns.Add(new DataColumn("Display", typeof(string)));
+                songlistDataTable.Columns.Add(new DataColumn("Song", typeof(string)));
+
+                foreach (string songPath in songsDirs)
+                {
+                    string song = songPath.Replace(osuSongsDir, "");
+
+                    DataRow row = songlistDataTable.NewRow();
+                    row[0] = Regex.Replace(song, "^\\d+\\s*", "");
+                    row[1] = song;
+                    songlistDataTable.Rows.Add(row);
+                }
+
+                songsList.DataSource = songlistDataTable;
+                songsList.DisplayMember = "Display";
+                songsList.ValueMember = "Song";
+
+                songlistDataTable.DefaultView.Sort = "Display asc";
+
+                selectRandom();
+            }
+            catch (Exception) {
+                MessageBox.Show("無法讀取 osu! 歌曲");
+                System.Environment.Exit(0);
+            }
+        }
+
+        void selectRandom() {
+            Random rnd = new Random();
+            songsList.SelectedItem = songsList.Items[rnd.Next(0, songsList.Items.Count - 1)];
+        }
+
+        void trimMp3(string input, string output, int startOffset, int endOffset){
+
+            using (Mp3FileReader reader = new Mp3FileReader(input)){
+                System.IO.FileStream _fs = new System.IO.FileStream(output, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+
+                Mp3Frame mp3Frame;
+                do
+                {
+                    mp3Frame = reader.ReadNextFrame();
+                    if (mp3Frame == null) return;
+                    if ((int)reader.CurrentTime.TotalSeconds < startOffset) continue;
+                    if ((int)reader.CurrentTime.TotalSeconds >= endOffset) break;
+
+                    _fs.Write(mp3Frame.RawData, 0, mp3Frame.RawData.Length);
+
+                } while (mp3Frame != null);
+                _fs.Close();
+            }
+        }
+
+
+
+        public void loopMp3()
+        {
+            try
+            {
+                using (var ms = File.OpenRead(playerSong))
+                using (var rdr = new Mp3FileReader(ms))
+                using (var wavStream = WaveFormatConversionStream.CreatePcmStream(rdr))
+                using (var baStream = new BlockAlignReductionStream(wavStream))
+                using (var waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+                {
+                    //rdr.Skip();
+                    wavStream.Skip(playerPreviewTime);
+                    waveOut.Init(baStream);
+                    waveOut.Play();
+                    while (waveOut.PlaybackState == PlaybackState.Playing && !closing)
+                    {
+                        Thread.Sleep(10);
+
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            playerThread.Abort();
+        }
+
+        private void songSelected(){
+            string selected = (songsList.SelectedItem != null) ? songsList.SelectedValue.ToString() : "";
+            string songPath = osuSongsDir + selected; if (!Directory.Exists(songPath)) return;
+            string[] files = Directory.GetFiles(songPath);
+
+
+            string audioFilename = "-1";
+            string imageFilename = "";
+            int previewTime = 0;
+
+
+            foreach (string file in files)
+            {
+                if (Regex.IsMatch(file, "osu$"))
+                {
+                    StreamReader osu = new StreamReader(file);
+                    string line;
+                    do
+                    {
+                        line = osu.ReadLine(); if (line == null) break;
+                        if (Regex.IsMatch(line, "^AudioFilename:")) audioFilename = Regex.Replace(line, "^AudioFilename:\\s*", "");
+                        if (Regex.IsMatch(line, "^PreviewTime:")) previewTime = Int32.Parse(Regex.Replace(line, "^PreviewTime:\\s*", ""));
+                        if (Regex.IsMatch(line, "^0,0,")) imageFilename = Regex.Replace(line, "^0,0,\\s*\"(.+)\"$", "$1");
+
+                    } while (line != null);
+                    break;
+                }
+            }
+            try
+            {
+                Image img = new Bitmap(songPath + "/" + imageFilename);
+                songImage.Image = img;
+            }
+            catch (Exception) { songImage.Image = null; }
+
+            btn_saveRingtone.Enabled = (audioFilename != "-1");
+
+            playerPreviewTime = (int)Math.Floor((double)previewTime / 1000);
+            string newSong = songPath + "/" + audioFilename;
+
+            if (newSong == playerSong) return;
+
+            playerSong = newSong;
+
+            if (playerThread != null && playerThread.IsAlive) playerThread.Abort();
+            playerThread = new Thread(this.loopMp3);
+            playerThread.Start();
+        }
+
+
+        private void songsList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            songSelected();
+        }
+
+        private void btn_saveRingtone_Click(object sender, EventArgs e)
+        {
+            saveAsRingtone();
+        }
+
+
+        private void songsList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            saveAsRingtone();
+        }
+
+        
+        private void saveAsRingtone(){
+            try
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.FileName = songsList.SelectedValue.ToString();
+                saveFileDialog1.DefaultExt = ".mp3";
+                saveFileDialog1.Filter = "MP3 files (*.mp3)|*.mp3";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    trimMp3(playerSong, saveFileDialog1.FileName, playerPreviewTime, playerPreviewTime+60);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //songsList.DataSour
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selectRandom();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.plurk.com/Lackneets");
+        }
+
+
+    }
+}
